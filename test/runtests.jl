826eeea5993d0e7be614177708d49d437310ae3f
Gustavo Landfried <gustavolandfried@gmail.com>
2020-09-02 17:14:13 -0300

    Write synthetic scenarios at runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -109,7 +109,7 @@ using JLD2
         @test isapprox(post[1][2],ttt.Gaussian(15.000,0.9916),1e-3)
         @test isapprox(post[2][1],ttt.Gaussian(30.000,1.9320),1e-3)
     end
-    @testset "Evidence" begin
+    @testset "Game evidence" begin
         
         @testset "1vs1" begin
             ta = [ttt.Rating(25.,1e-7)]
@@ -144,7 +144,6 @@ using JLD2
             println("Corregir la evidencia multiequipos para que sume 1")
             @test  isapprox(proba, 1.49999991)
         end
-        
     end
     @testset "Forget" begin
         r = ttt.Rating(25.,1e-7)
@@ -238,14 +237,6 @@ using JLD2
             @test isapprox(lc["cj"][end][2],ttt.Gaussian(25.000,5.420),2)
         end
     end
-    @testset "Save" begin
-        events = [ [["aj"],["bj"]],[["bj"],["cj"]], [["cj"],["aj"]] ]
-        results = [[0,1],[0,1],[0,1]]    
-        h = ttt.History(events, results, [5,6,7])
-        ttt.convergence(h)
-        @save "test_save.jld2" h
-        lc = ttt.learning_curves(g)
-    end
     @testset "CSV Same strength" begin 
         events = [ [["aj"],["bj"]],[["bj"],["cj"]], [["cj"],["aj"]] ,[["aj"],["bj"]],[["bj"],["cj"]], [["cj"],["aj"]]]
         results = [[0,1],[0,1],[0,1],[0,1],[0,1],[0,1]]    
@@ -291,20 +282,27 @@ using JLD2
             priors[k] = ttt.Rating(0., 3.0, 0.5, 0.0, k ) 
         end
         h = ttt.History(events, results, [1,2,3,4,5,6, 1,2,3,4,5,6, 7,7,7,7,7,7,7,7], priors)
+        trueSkill_evidence = ttt.log_evidence(h)
         lc_ts = ttt.learning_curves(h)
         ttt.convergence(h)
+        throughTime = ttt.log_evidence(h)
+        evs = [e for b in h.batches for e in b.evidences]
         lc_ttt = ttt.learning_curves(h)
-        1-ttt.cdf(lc_ttt["aj"][1][2] -lc_ttt["bj"][1][2],0.) 
-        1-ttt.cdf(lc_ttt["cj"][1][2] -lc_ttt["bj"][1][2],0.) 
         
-        1-ttt.cdf(lc_ttt["ai"][1][2] -lc_ttt["bi"][1][2],0.) 
-        1-ttt.cdf(lc_ttt["ci"][1][2] -lc_ttt["bi"][1][2],0.) 
+        # Predicciones
+        Nbeta = ttt.Gaussian(0.0,0.5)
+        1-ttt.cdf(lc_ttt["aj"][1][2]+Nbeta -lc_ttt["bj"][1][2]+Nbeta,0.) 
+        1-ttt.cdf(lc_ttt["cj"][1][2]+Nbeta -lc_ttt["bj"][1][2]+Nbeta,0.) 
+        1-ttt.cdf(lc_ttt["ai"][1][2]+Nbeta -lc_ttt["bi"][1][2]+Nbeta,0.) 
+        1-ttt.cdf(lc_ttt["ci"][1][2]+Nbeta -lc_ttt["bi"][1][2]+Nbeta,0.) 
+        1-ttt.cdf(lc_ttt["bj"][1][2]+Nbeta -lc_ttt["bi"][1][2]+Nbeta,0.) 
+        1-ttt.cdf(lc_ttt["aj"][1][2]+Nbeta -lc_ttt["ai"][1][2]+Nbeta,0.) 
         
-        1-ttt.cdf(lc_ttt["bj"][1][2] -lc_ttt["bi"][1][2],0.) 
-        1-ttt.cdf(lc_ttt["aj"][1][2] -lc_ttt["ai"][1][2],0.) 
+        # Diferencia media
+        #1-ttt.cdf(ttt.Gaussian(15.0,sqrt(2*15.0^2)),0.) 
+        1-ttt.cdf(ttt.Gaussian((lc_ttt["aj"][1][2]-lc_ttt["ai"][1][2]).mu,sqrt(2*0.5^2)),0.) 
         
-        1-ttt.cdf(ttt.Gaussian(0.5,0.5),0.) 
-        1-ttt.cdf(ttt.Gaussian(0.33725,0.5),0.) 
+        1-ttt.cdf(ttt.Gaussian(0.33725,sqrt(2*0.5^2) ),0.) 
         
         
         df = DataFrame(mu_a_ts = [ N.mu for (k,N) in lc_ts["aj"] if k != 7]
@@ -342,6 +340,92 @@ using JLD2
         CSV.write("same_strength_two_groups.csv", df; header=true)
         @test true
     end
-    
+    @testset "Write smoothing messages"
+        events = [ [["a"],["b"]], [["a"],["b"]]]
+        results = [[0,1],[1,0]]
+        times = [1,2]
+        priors = Dict{String,ttt.Rating}()
+        for k in ["a", "b"]
+            priors[k] = ttt.Rating(0., 3.0, 0.5, 0.0, k ) 
+        end
+        h = ttt.History(events, results, times, priors)
+        fp_a = Vector{ttt.Gaussian}()
+        bp_a = Vector{ttt.Gaussian}() 
+        lh_a = Vector{ttt.Gaussian}()
+        wp_a = Vector{ttt.Gaussian}()
+        wp_b = Vector{ttt.Gaussian}()
+        p_a =  Vector{ttt.Gaussian}()
+        
+        push!(fp_a, h.batches[1].prior_forward["a"].N)
+        push!(bp_a, h.batches[1].prior_backward["a"])
+        push!(lh_a, h.batches[1].likelihood["a"][1])
+        push!(wp_a, fp_a[end]*bp_a[end])
+        push!(p_a, wp_a[end]*lh_a[end])
+        push!(wp_b, h.batches[1].prior_forward["b"].N*h.batches[1].prior_backward["b"])
+        for _ in 1:10
+            ttt.iteration(h)
+            push!(fp_a, h.batches[1].prior_forward["a"].N)
+            push!(bp_a, h.batches[1].prior_backward["a"])
+            push!(lh_a, h.batches[1].likelihood["a"][1])
+            push!(wp_a, fp_a[end]*bp_a[end])
+            push!(p_a, wp_a[end]*lh_a[end])
+            push!(wp_b, h.batches[1].prior_forward["b"].N*h.batches[1].prior_backward["b"])
+        end
+        
+        df = DataFrame(fp_mu = [N.mu for N in fp_a]
+                      ,fp_sigma = [N.sigma for N in fp_a]
+                      ,bp_mu = [N.mu for N in bp_a]
+                      ,bp_sigma = [N.sigma for N in bp_a]
+                      ,lh_mu = [N.mu for N in lh_a]
+                      ,lh_sigma = [N.sigma for N in lh_a]
+                      ,wp_mu = [N.mu for N in wp_a]
+                      ,wp_sigma = [N.sigma for N in wp_a]
+                      ,p_mu = [N.mu for N in p_a]
+                      ,p_sigma = [N.sigma for N in p_a]
+                      ,wp_opponent_mu = [N.mu for N in wp_b]
+                      ,wp_opponent_sigma = [N.sigma for N in wp_b])
+        CSV.write("smoothing.csv", df; header=true)
+        
+    end
+    @testset "TTT vs TrueSkill evidence"
+        
+        events = Array{Array{Array{String,1},1},1}()
+        results =  Array{Array{Int64,1},1}()
+        times = Int64[]
+        ts_log_evidence = Float64[]
+        ttt_log_evidence = Float64[]
+        
+        ts_last = Float64[]; ts_midle = Float64[]; ts_second = Float64[]
+        ttt_last = Float64[]; ttt_midle = Float64[]; ttt_second = Float64[]
+        for i in 0:127
+            push!(events ,  [["a"],["b"]], [["a"],["b"]])
+            push!(results ,  [0,1],[1,0] )
+            push!(times,  i*2+1, i*2+2 )
+            priors = Dict{String,ttt.Rating}()
+            for k in ["a", "b"]
+                priors[k] = ttt.Rating(0., 3.0, 0.5, 0.0, k ) 
+            end
+            h = ttt.History(events, results, times, priors)
+            push!(ts_log_evidence , ttt.log_evidence(h))
+            push!(ts_last, h.batches[end].evidences[1]); push!(ts_midle, h.batches[i*2+1].evidences[1]); 
+            push!(ts_second, h.batches[2].evidences[1]);
+            ttt.convergence(h)
+            push!(ttt_last, h.batches[end].evidences[1]); push!(ttt_midle, h.batches[i*2+1].evidences[1]); 
+            push!(ttt_log_evidence, ttt.log_evidence(h))
+            push!(ttt_second, h.batches[2].evidences[1])
+        end
+       df = DataFrame(ts_log_evidence = ts_log_evidence
+                     ,ttt_log_evidence = ttt_log_evidence
+                     ,ts_last = ts_last
+                     ,ts_midle = ts_midle
+                     ,ts_second = ts_second
+                     ,ttt_second = ttt_second
+                     ,ttt_last = ttt_last
+                     ,ttt_midle = ttt_midle )
+            
+        CSV.write("ttt_vs_ts.csv", df; header=true)
+ 
+    end
+        
 end
 
