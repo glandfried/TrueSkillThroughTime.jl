530cb2baa2d0b8ddfff74733b23cfdc8a85a58b7
Gustavo Landfried <gustavolandfried@gmail.com>
2021-03-25 17:18:31 -0300

    Examples: change input url, and fix some julia syntax

--- example/atp_optimization.jl
+++ example/atp_optimization.jl
@@ -5,22 +5,24 @@ include("../src/TrueSkill.jl")
 using .TrueSkill
 global const ttt = TrueSkill
 
-data = CSV.read("input/history.csv")
+data = CSV.read("input/history.csv", DataFrame)
 times = Dates.value.(data[:,"time_start"] .- Date("1900-1-1"))
 composition = [ r.double == "t" ? [[r.w1_id,r.w2_id],[r.l1_id,r.l2_id]] : [[r.w1_id],[r.l1_id]] for r in eachrow(data) ]   
 
 sigmas = [i for i in 0.3:0.1:1.2]
 gammas = [i for i in 0.0025:0.0025:0.025]
 
-evidence = DataFrame(zeros(length(sigmas),length(gammas)))
-names!(evidence, Symbol.(string.(sigmas)))
-evidence[!,:rownames] = gammas
+df = DataFrame()
+df[!,:rownames] = gammas
+for s in sigmas
+    df[!,string(s)] .= 0.0
+end
 
 for s in 1:length(sigmas)
     for g in 1:length(gammas)
         h = ttt.History(composition=composition, times = times, sigma = sigmas[s], gamma = gammas[g])
         ttt.convergence(h,epsilon=0.01, iterations=10)
-        evidence[g,s] = ttt.log_evidence(h)
+        df[g,string(sigmas[s])] = ttt.log_evidence(h)
     end
 end
 
